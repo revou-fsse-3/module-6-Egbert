3a5c905d742e1d210acfcc6a412662832a40bdc1
Egbert Felica Wibianto <137465044+Orang9@users.noreply.github.com>
2024-02-23 10:02:21 +0700

    Update __init__.py

--- pyhton-flask-Revou/app/__init__.py
+++ pyhton-flask-Revou/app/__init__.py
@@ -15,7 +15,7 @@ DATABASE_NAME = os.getenv('DATABASE_NAME')
 DATABASE_PORT = os.getenv('DATABASE_PORT')
 app.config["SQLALCHEMY_DATABASE_URI"] = f"{DATABASE_TYPE}://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
-# db.init_app(app)
+db.init_app(app)
 migrate.init_app(app, db)
 
 app.register_blueprint(animal_route.animal_blueprint, url_prefix="/animal")
