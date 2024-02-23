d2b2436a893396fb55b7c16cfaf6e98554b9734b
Egbert Felica Wibianto <egbert.fewi@gmail.com>
2024-02-23 18:09:24 +0700

    update unit test controller animal and add create animal request

--- pyhton-flask-Revou/tests/controller/test_animal_controller.py
+++ pyhton-flask-Revou/tests/controller/test_animal_controller.py
@@ -1,47 +1,88 @@
-def test_get_all_list_animal(test_app, mocker):
+from app import db
+from app.service.animal_service import Animal_Service
+
+def test_get_animal(test_app, mocker):
+    mock_animal_data = [
+        {
+            "id": 1,
+            "species": "Tiger",
+            "age": 10,
+            "gender": "Male"
+        },
+    ]
+    mocker.patch.object(Animal_Service, 'get_animal', return_value=mock_animal_data)
+    
+    # Act
     with test_app.test_client() as client:
-        # Act
         response = client.get("/animal/")
 
-    assert len(response.json['data']) == 5
-
-
-def test_put_animal_success(test_app):
-    data = {
-        "species": "Elephant",
-        "age": 12,
-        "gender": "Female"
-    }
-    with test_app.test_client() as client:
-        # Act
-        response = client.put("/animal/2", json=data)
-
     assert response.status_code == 200
+    assert len(response.json['data']) == len(mock_animal_data)
+    assert response.json['data'] == mock_animal_data
 
-def test_put_animal_failed(test_app):
-    data = {}
+def test_post_animal(test_app, mocker):
+    data = [
+        {
+            "id": 1,
+            "species": "Tiger",
+            "age": 10,
+            "gender": "Male"
+        },
+    ]
+    mocker.patch.object(Animal_Service, 'create_animal', return_value=data)
+    
+    # Act
     with test_app.test_client() as client:
-        # Act
-        response = client.put("/animal/1", json=data)
+        response = client.get("/animal/", json=data)
 
-    assert response.status_code == 400
-
-def test_delete_animal_success(test_app):
-    data = {
-        "species": "Panda",
+    expected_response = {
+        "species": "Tiger",
         "age": 10,
         "gender": "Male"
     }
-    with test_app.test_client() as client:
-        # Act
-        response = client.put("/animal/9", json=data)
 
     assert response.status_code == 200
+    assert len(response.json['data']) == len(mock_animal_data)
+    print(data)
+    assert response.json['data']["name"] == "Tiger"
 
-def test_delete_animal_failed(test_app):
-    data = {}
-    with test_app.test_client() as client:
-        # Act
-        response = client.put("/animal/9", json=data)
 
-    assert response.status_code == 400
+# def test_put_animal_success(test_app):
+#     data = {
+#         "species": "Elephant",
+#         "age": 12,
+#         "gender": "Female"
+#     }
+#     with test_app.test_client() as client:
+#         # Act
+#         response = client.put("/animal/2", json=data)
+
+#     assert response.status_code == 200
+
+# def test_put_animal_failed(test_app):
+#     data = {}
+#     with test_app.test_client() as client:
+#         # Act
+#         response = client.put("/animal/1", json=data)
+
+#     assert response.status_code == 400
+
+# def test_delete_animal_success(test_app):
+#     data = {
+#         "species": "Panda",
+#         "age": 10,
+#         "gender": "Male"
+#     }
+#     with test_app.test_client() as client:
+#         # Act
+#         response = client.put("/animal/9", json=data)
+
+#     assert response.status_code == 200
+
+# def test_delete_animal_failed(test_app):
+#     data = {}
+#     with test_app.test_client() as client:
+#         # Act
+#         response = client.put("/animal/9", json=data)
+
+#     assert response.status_code == 400
